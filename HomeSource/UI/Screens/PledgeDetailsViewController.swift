88003accf7022b4efc39d1dd16c9963c1e7dcef0
Gary Butcher <garyb@apadmi.com>
2016-04-22 10:22:59 +0100

    Add social icon for pledge. Add app icon

--- HomeSource/UI/Screens/PledgeDetailsViewController.swift
+++ HomeSource/UI/Screens/PledgeDetailsViewController.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class PledgeDetailsViewController: UIViewController {
     
+    var goal : Goal?
+    var value : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Slider listener for value change
+    }
+    
+    func shareDonation() {
+        
+        guard let goal = self.goal else {
+            print("No goal set");
+            return
+        }
+        
+        let shareTitle = "I have just donated " + goal.formatString(value)
+        let url = NSURL(string: "http://www.barnabus-manchester.org.uk")!
+        let image: UIImage = UIImage(named: "SocialIcon")!
+        
+        let shareVC: UIActivityViewController = UIActivityViewController(activityItems: [image, shareTitle, url], applicationActivities: nil)
+        self.presentViewController(shareVC, animated: true, completion: nil)
+    }
+    
 }
